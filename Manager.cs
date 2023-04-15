b965630b2c18160848ebbe8acae8e5e0cc12ea24
Zarpelon616 <100882885+Zarpelon616@users.noreply.github.com>
2023-04-15 15:31:53 -0300

    Create Manager.cs

---
+++ Manager.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace atividade01.classes
+
+{
+    public class Manager: Person
+    {
+      public bool IsWorker;
+        
+    }
+}
