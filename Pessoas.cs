c3d64847ef6025684a7578285cbaf274aa80212e
Zarpelon616 <100882885+Zarpelon616@users.noreply.github.com>
2023-04-15 15:31:31 -0300

    Create Pessoas.cs

---
+++ Pessoas.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace atividade01.classes
+{
+    public class Person
+    {
+
+        public string Name;
+
+        public DateTime Born;
+
+        public string ID;
+
+        public int ServiceHours;
+
+        public string Cargo;
+
+    }
+}
